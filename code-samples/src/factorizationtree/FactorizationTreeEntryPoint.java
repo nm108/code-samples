df675d3ed7b69c3c31dd0096697e73d8742a4df6
Andrzej Wysocki <neomahakala108@gmail.com>
2026-04-16 17:53:21 +0200

    removed obsolete, unneeded code

--- code-samples/src/factorizationtree/FactorizationTreeEntryPoint.java
+++ code-samples/src/factorizationtree/FactorizationTreeEntryPoint.java
@@ -5,27 +5,18 @@ public class FactorizationTreeEntryPoint {
 	// entry point.
 	
 	public static void main(String[] args) {
-		boolean useThreshold = false;
-		boolean cutBounds = false;
+		boolean useThreshold = true;
+		boolean cutBounds = true;
 		// long num = 17;
 
 //		NumberFactored n = new NumberFactored(108, useThreshold);
-		 NumberFactored n = new NumberFactored(36*36*10, useThreshold);
+		 NumberFactored n = new NumberFactored(36*36*7, useThreshold);
 		// NumberFactored n1 = new NumberFactored(num, useThreshold);
 		// NumberFactored n = new NumberFactored(n1);
 
 		System.out.println(n.getGeneralStateAsStringBuilder(cutBounds));
 		System.out.println(n.getTreeAsStringBuilder());
 
-		// printDebugInfo();
 	}
 
-	// utility method for debugging.
-	
-	public static void printDebugInfo() {
-		NumberFactored n;
-		StringBuilder sb = new StringBuilder();
-		
-		System.out.println(sb.toString());
-	}
 }
